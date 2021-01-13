21279d6ee16bb9a8596dd3f78b4dec7393b5c497
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-13 19:46:39 +0530

    13012021v3

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # React Guide
 
 npm install -g create-react-app
-create-react-app react-complete-guide --scripts-version 1.1.5
 
+create-react-app react-complete-guide --scripts-version 1.1.5
+cd react-complete-guide/
+npm start
+
+create-react-app cricket-scorer --scripts-version 1.1.5 && cd cricket-scorer/
 npm start
--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import {useState} from 'react';
 // eslint-disable-next-line
 import Person from './Person/Person.js';
 import { Project } from './Portfolio/Portfolio.js';
@@ -9,19 +10,92 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1 className="App-title">Welcome to React</h1>
-				<p>This is my first project.</p>
-				<Person />
-				<Person />
-				<Person />
-				<Person />
-				<Person />
+				<p>This is my first project</p>
 				<div className="all-projects">
 					<Project title="Machine Learning with Job Data" desc="Web-scrapping indeed.com data, I have found the best possible jobs in a locality. " details="Programming Language -> Python. Front-End -> Dash. ML/DL -> Classification and Regression Problem Statement."/>
 					<Project title="Machine Learning with Audio Data" desc="Generating new folk music using LSTMs." details="Programming Language -> Python. Front-End -> Django. Back-End -> Django. ML/DL -> LSTM, Auto Encoder, GANs"/>
 				</div>
 			</div>
-		);
+		)
 	}
 }
+
+// Class Based State
+// class App extends Component {
+// 	state = {
+// 		persons: [
+// 			{name: 'Alvyn', age: 24},
+// 			{name: 'Max', age: 28},
+// 		]
+// 	}
+
+// 	switchNameHandler = () => {
+// 		// console.log('Was Clicked!');
+// 		// DONT DO THIS: this.state.persons[0].name = 'Alvyn Abranches';
+// 		this.setState({
+// 			persons: [
+// 				{name: 'Alvyn Abranches', age: 24},
+// 				{name: 'Maximilian', age: 28},
+// 			]
+// 		});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="App">
+// 				<h1 className="App-title">Welcome to React</h1>
+// 				<p>This is my first project.</p>
+// 				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+// 				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+// 				<Person name="Joy"/>
+// 				<Person age="20"/>
+// 				<Person />
+// 				<button onClick={this.switchNameHandler}>Switch Name</button>
+// 				<div className="all-projects">
+// 					<Project title="Machine Learning with Job Data" desc="Web-scrapping indeed.com data, I have found the best possible jobs in a locality. " details="Programming Language -> Python. Front-End -> Dash. ML/DL -> Classification and Regression Problem Statement."/>
+// 					<Project title="Machine Learning with Audio Data" desc="Generating new folk music using LSTMs." details="Programming Language -> Python. Front-End -> Django. Back-End -> Django. ML/DL -> LSTM, Auto Encoder, GANs"/>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// Function Based State
+// const app = () => {
+// 	const [ personsState, setPersonsState] = useState({
+// 		persons: [
+// 			{name: 'Alvyn', age: 24},
+// 			{name: 'Max', age: 28},
+// 		]
+// 	});
+
+// 	const switchNameHandler = () => {
+// 		// console.log('Was Clicked!');
+// 		// DONT DO THIS: this.state.persons[0].name = 'Alvyn Abranches';
+// 		setPersonsState({
+// 			persons: [
+// 				{name: 'Alvyn Abranches', age: 24},
+// 				{name: 'Maximilian', age: 28},
+// 			]
+// 		});
+// 	}
+
+// 	return (
+// 		<div className="App">
+// 			<h1 className="App-title">Welcome to React</h1>
+// 			<p>This is my first project.</p>
+// 			<Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+// 			<Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+// 			<Person name="Joy"/>
+// 			<Person age="20"/>
+// 			<Person />
+// 			<button onClick={switchNameHandler}>Switch Name</button>
+// 			<div className="all-projects">
+// 				<Project title="Machine Learning with Job Data" desc="Web-scrapping indeed.com data, I have found the best possible jobs in a locality. " details="Programming Language -> Python. Front-End -> Dash. ML/DL -> Classification and Regression Problem Statement."/>
+// 				<Project title="Machine Learning with Audio Data" desc="Generating new folk music using LSTMs." details="Programming Language -> Python. Front-End -> Django. Back-End -> Django. ML/DL -> LSTM, Auto Encoder, GANs"/>
+// 			</div>
+// 		</div>
+// 	);
+// }
 
 export default App;
