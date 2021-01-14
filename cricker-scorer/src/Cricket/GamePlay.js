5f98a96e4bc8071e65c65a3b5dba272aef8816ba
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-14 21:49:59 +0530

    14012021v2

---
+++ cricker-scorer/src/Cricket/GamePlay.js
@@ -0,0 +1,57 @@
+team1 = {
+    runs: 0,
+    balls: 0,
+    extras: 0,
+    batsmen: {
+        1: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        2: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        3: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        4: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        5: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        6: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        7: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        8: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        9: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        10: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        11: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+    },
+    bowler: {
+        1: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        2: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        3: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        4: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        5: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        6: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        7: {runs: 0, balls: 0, maidens: 0, extras: 0},
+    }
+}
+
+team2 = {
+    runs: 0,
+    balls: 0,
+    extras: 0,
+    batsmen: {
+        1: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        2: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        3: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        4: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        5: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        6: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        7: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        8: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        9: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        10: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+        11: {runs: 0, balls: 0, ones: 0, twos: 0, threes: 0, fours: 0, six: 0},
+    },
+    bowler: {
+        1: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        2: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        3: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        4: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        5: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        6: {runs: 0, balls: 0, maidens: 0, extras: 0},
+        7: {runs: 0, balls: 0, maidens: 0, extras: 0},
+    }
+}
+
+export { team1 as Team1, team2 as Team2 }
